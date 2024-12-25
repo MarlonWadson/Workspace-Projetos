abe20ab22ba022a68faccde97430e47384b19610
MarlonWadson <marlonwadson115@gmail.com>
2024-12-25 17:12:31 -0300

    Classe que representa as coordenadas

--- Back-End/Ling.Python/minecraft_coordenates.py
+++ Back-End/Ling.Python/minecraft_coordenates.py
@@ -13,6 +13,31 @@ db_path = os.path.join(os.path.dirname(__file__), "Coordenadas_Mine.db")
 db = create_engine(f"sqlite:///{db_path}") #Criando o arquivo do BD
 Base = declarative_base()
 
+class Coordenadas(Base):
+    """Classe de Coordenadas."""
+    __tablename__ = "Coordenadas"
+    server = Column('Servidor', String, primary_key=True)
+    local = Column("Local", String)
+    coor_x = Column("X", String)
+    coor_y = Column("Y", String)
+    coor_z = Column("Z", String)
+
+    def __init__(self, server, local, coor_x, coor_y, coor_z) -> None:
+        """Inicializa os atributos da classe.
+
+        Args:
+            server (string): Nome do servidor.
+            local (string): Nome do local.
+            coor_x (string): Coordenada X.
+            coor_y (string): Coordenada Y.
+            coor_z (string): Coordenada Z.
+        """
+        self.server = server
+        self.local = local
+        self.coor_x = coor_x
+        self.coor_y = coor_y
+        self.coor_z = coor_z
+
 #Criando as tabelas no BD
 Base.metadata.create_all(bind=db)
 
